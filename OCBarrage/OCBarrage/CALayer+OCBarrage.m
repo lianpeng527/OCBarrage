cfeb9c66ca4a5d5d9bf18e98a81baa38471aa213
lianpeng527 <baabaalamb@163.com>
2024-01-04 19:48:30 +0800

    Update CALayer+OCBarrage.m
    update

--- OCBarrage/OCBarrage/CALayer+OCBarrage.m
+++ OCBarrage/OCBarrage/CALayer+OCBarrage.m
@@ -11,14 +11,23 @@
 @implementation CALayer (OCBarrage)
 
 - (UIImage *)convertContentToImageWithSize:(CGSize)contentSize {
-    UIGraphicsBeginImageContextWithOptions(contentSize, 0.0, [UIScreen mainScreen].scale);
-    //self为需要截屏的UI控件 即通过改变此参数可以截取特定的UI控件
-    [self renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image= UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return image;
-}
+//    UIGraphicsBeginImageContextWithOptions(contentSize, 0.0, [UIScreen mainScreen].scale);
+//    //self为需要截屏的UI控件 即通过改变此参数可以截取特定的UI控件
+//    [self renderInContext:UIGraphicsGetCurrentContext()];
+//    UIImage *image= UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    
+//    return image;
+    UIGraphicsImageRendererFormat *format = [[UIGraphicsImageRendererFormat alloc] init];
+     format.opaque = NO;
+     format.scale = [UIScreen mainScreen].scale;
+     UIGraphicsImageRenderer *render = [[UIGraphicsImageRenderer alloc] initWithSize:contentSize format:format];
+     UIImage *image = [render imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+         CGRect rect = CGRectMake(0, 0, contentSize.width, contentSize.height);
+         UIRectFill(rect);
+     }];
 
+     return image;
+}
 
 @end
